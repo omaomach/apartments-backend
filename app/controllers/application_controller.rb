c1f51ce1162dc6da399c94531b2d006072cbb166
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-11-10 03:02:37 +0300

    enabled CORS preflight requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   before do
-    response.headers["Access-control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+  end
+
+  #enable CORS preflight requests
+  options "*" do
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, UPDATE, DELETE"
   end
   
   # Add your routes here
